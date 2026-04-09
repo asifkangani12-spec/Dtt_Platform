f0b7532f02b77d33101ff8dfe78121b0bb21d65c
asifkangani12-spec <asifkangani12@gmail.com>
2026-04-09 18:14:14 +0530

    Update DeleteService.java

--- onboarding-transaction-handler/src/main/java/ug/daes/OnBoardingTransactionHandler/service/DeleteService.java
+++ onboarding-transaction-handler/src/main/java/ug/daes/OnBoardingTransactionHandler/service/DeleteService.java
@@ -80,7 +80,6 @@ public class DeleteService {
 			e.printStackTrace();
 			return exceptionHandlerUtil.handleHttpException(e);
 		} catch (ResourceAccessException e) {
-			e.printStackTrace();
 			return exceptionHandlerUtil.handleResourceAccessException(e);
 		} catch (Exception e) {
 			return ExceptionHandlerUtil.handleGenericException(e);
